a853fc03eda0607f9561ee3b5a65494a19ecdf9e
Vitor Morais Siqueira <42281886+vitormsiqueira@users.noreply.github.com>
2020-08-29 14:45:44 -0400

    Add files

---
+++ js/todo.js
@@ -0,0 +1,55 @@
+var addTodo = function(){
+    var todo = document.getElementById('todo').value; 
+    var todos = getTodos(); 
+    if(todo){ 
+        todos.push(todo);
+        localStorage.setItem('todos', JSON.stringify(todos));
+        document.getElementById('todo').value = '';
+    }
+    document.location.reload(true);
+}
+var getTodos = function(){
+    var todos = []; 
+    var todos_string = localStorage.getItem('todos');
+    if(todos_string != null){ 
+        return JSON.parse(todos_string);
+    }
+}
+var showTodos = function(){
+    var todos = getTodos();
+    var html = '<ul>';
+    todos.forEach(function(elemento, index){
+        html += '<li> <label class="container"><input type="checkbox"> <span class="checkmark"></span> </label>' + elemento + '<button class="remove" id="'+ index +'">Remover</button></li>'; 
+    });
+    html += '</ul>'; 
+    document.getElementById('todos').innerHTML = html; 
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++){ 
+        buttons[i].addEventListener('click', removeTodo);
+    };
+}
+var removeTodo = function(){
+    var id = this.getAttribute('id');
+    var todos = getTodos(); 
+    todos.splice(id, 1);
+    localStorage.setItem('todos', JSON.stringify(todos)); 
+    document.location.reload(true);
+}
+var hasTodo = function(){ 
+    var todos = getTodos();
+    if(todos == ''){
+        text = '<h2>Não há tarefas cadastradas!</h2>';
+        document.getElementById('msg').innerHTML = text;
+    } else {
+        text = '<h2>Tarefas pendentes:</h2>';
+        document.getElementById('msg').innerHTML = text;
+    }
+}
+document.getElementById('add').addEventListener('click', addTodo);
+window.addEventListener('keydown', function(event){ 
+    if(event.keyCode == 13){
+        addTodo();
+    };
+}); 
+hasTodo(); 
+showTodos(); 
