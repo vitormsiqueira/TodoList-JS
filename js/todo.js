9d997be69592f8bdbe480653adffed6dbe763e75
Vitor Morais Siqueira <vitormsiqueira@users.noreply.github.com>
2020-09-01 22:12:51 -0400

    checkbox state management

--- js/todo.js
+++ js/todo.js
@@ -19,7 +19,7 @@ var showTodos = function(){
     var todos = getTodos();
     var html = '<ul>';
     todos.forEach(function(content, index){
-        html += '<div class="container-blur"> <li> <label class="container"><input type="checkbox" name="mycheck" onClick="CheckBoxMarked()"> <span class="checkmark"></span> </label>' + content + '<button class="remove" id="'+ index +'"><i class="ic-delete"></i></button></li></div>'; 
+        html += '<div class="container-blur"> <li> <label class="container"><input type="checkbox" name="checkBoxMarked" onClick="checkBoxMarked(this)"> <span class="checkmark"></span> </label>' + content + '<button class="remove" id="'+ index +'"><i class="ic-delete"></i></button></li></div>'; 
     });
     html += '</ul>'; 
     document.getElementById('todos').innerHTML = html; 
@@ -52,11 +52,18 @@ window.addEventListener('keydown', function(event){
     };
 }); 
 
-function CheckBoxMarked(){
+var checkBoxMarked = function(checkbox){
 
-    var var_check = document.getElementsByName('mycheck');
+    //var var_check = document.getElementsByName('mycheck');
 
-    alert("Tarefa marcada como concluida");
+    if (checkbox.checked)
+    {
+        alert("Tarefa marcada como concluída");
+    } else {
+        alert("Tarefa marcada para fazer");
+    }
+
+    //alert("Tarefa marcada como concluida");
   
     
   }
